8bb1a6459082f11e86db75ab015cdc9b6ffc9d8b
Germain Hirwa <90903640+germainhirwa@users.noreply.github.com>
2023-11-05 18:41:12 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
-from scoreboard import Scoreboard
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
